d311c153cb10f591a58691bcf2504cf4e94be410
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-08-05 18:46:27 +0300

    MOB IMP 3

--- src/components/invitation-comp/invitation-component.tsx
+++ src/components/invitation-comp/invitation-component.tsx
@@ -25,11 +25,19 @@ function InvitationComponent() {
           <h3>2025</h3>
         </div>
       </div>
-
+      <div className="form">
+        <h2>Please Fill Attendance Form</h2>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdhFEGw2ODivOYuhmY2zZMHizx6kzc-K7MNfDrme8a8-Hyn5A/viewform?embedded=true"
+          className="form-inp"
+        >
+          Loading…
+        </iframe>
+      </div>
       <div className="address">
-       
         <h3> Address: Rūpniecības iela 21</h3>
       </div>
+
       <div className="map">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2176.6468432014995!2d24.09940181153863!3d56.96591507344699!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfc052b4460d%3A0xd2b67f285c625886!2zUsWrcG5pZWPEq2JhcyBpZWxhIDIxLCBaaWVtZcS8dSByYWpvbnMsIFLEq2dhLCBMVi0xMDQ1!5e0!3m2!1sen!2slv!4v1754393364229!5m2!1sen!2slv"
