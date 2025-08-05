348ef8d44d8396b629e69c3ed43e9a2cb2d745df
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-08-05 21:03:24 +0300

    MOB IMP 5

--- src/components/invitation-comp/invitation-component.tsx
+++ src/components/invitation-comp/invitation-component.tsx
@@ -1,7 +1,37 @@
+import { useState, useRef, useEffect } from "react";
 import coupleImg from "../../assets/couple.png";
 import { Footer } from "../Footer";
 
 function InvitationComponent() {
+  const [isLocationOpen, setIsLocationOpen] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const map = useRef<HTMLDivElement | null>(null);
+  const form = useRef<HTMLDivElement | null>(null);
+
+  const toggleLocation = (): void => {
+    setIsLocationOpen((prev) => {
+      const newState = !prev;
+      if (!prev) {
+        setTimeout(() => {
+          map.current?.scrollIntoView({ behavior: "smooth" });
+        }, 100);
+      }
+      return newState;
+    });
+  };
+
+  const toggleForm = () => {
+    setIsFormOpen((prev) => {
+      const newState = !prev;
+      if (!prev) {
+        setTimeout(() => {
+          form.current?.scrollIntoView({ behavior: "smooth" });
+        }, 100);
+      }
+      return newState;
+    });
+  };
+
   return (
     <section className="invitation">
       <div className="img-box">
@@ -11,7 +41,7 @@ function InvitationComponent() {
         <div className="intro">
           <h2 className="arab">بسم الله الرحمن الرحيم</h2>
           <h2>
-            You Are Invited To Our Wedding <br /> A Beautiful Love Story Of{" "}
+            You Are Invited To Our Wedding <br /> The Beautiful Love Story Of{" "}
           </h2>
         </div>
         <div className="names">
@@ -21,27 +51,30 @@ function InvitationComponent() {
         </div>
         <div className="details">
           <h3>August</h3>
-          <h3>Friday | 15 | 11:30</h3>
+          <h3>Friday | 15 | 11:30 AM</h3>
           <h3>2025</h3>
         </div>
       </div>
-      <div className="form">
-        <h2>Please Fill Attendance Form</h2>
+      <button onClick={toggleForm} className="toggle-btn">
+        Attendance Form
+      </button>
+      <div ref={form} className={`form ${isFormOpen ? "open" : ""}`}>
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSdhFEGw2ODivOYuhmY2zZMHizx6kzc-K7MNfDrme8a8-Hyn5A/viewform?embedded=true"
-          className="form-inp"
+          className={`form-inp ${isFormOpen && "open"}`}
         >
           Loading…
         </iframe>
       </div>
-      <div className="address">
-        <h3> Address: Rūpniecības iela 21</h3>
-      </div>
 
-      <div className="map">
+      <button onClick={toggleLocation} className="toggle-btn">
+        Location
+      </button>
+      <div ref={map} className={`map ${isLocationOpen && "open"}`}>
+        <h3> Address: Rūpniecības iela 21</h3>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2176.6468432014995!2d24.09940181153863!3d56.96591507344699!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfc052b4460d%3A0xd2b67f285c625886!2zUsWrcG5pZWPEq2JhcyBpZWxhIDIxLCBaaWVtZcS8dSByYWpvbnMsIFLEq2dhLCBMVi0xMDQ1!5e0!3m2!1sen!2slv!4v1754393364229!5m2!1sen!2slv"
-          className="map-frame"
+          className={`map-frame ${isLocationOpen && "open"}`}
           loading="lazy"
         ></iframe>
       </div>
